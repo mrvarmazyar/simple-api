9723d19b0a71e323a2ea427d4a6a99a8d08a3065
Mohammad Varmazyar <m.varmazyar@alibaba.ir>
2019-05-27 21:39:04 +0430

    add snippets.py

---
+++ src/snippets.py
@@ -0,0 +1,28 @@
+import json
+
+import jsonschema
+
+from db import db_connection
+from users.models import User
+
+
+def create_tables():
+
+    # It will be an idempotent action: if there is already a corresponding table,
+    #   following would leave it untouched
+    db_connection.create_tables([User])
+
+
+def _load_json(file_path):
+    with open(file_path) as f:
+        content = json.load(f)
+    return content
+
+
+def validate_data(data, schema_path):
+    schema = _load_json(schema_path)
+
+    try:
+        jsonschema.validate(data, schema)
+    except jsonschema.exceptions.ValidationError as exc:
+        return {exc.path[0]: exc.message}
