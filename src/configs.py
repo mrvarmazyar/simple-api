fadea426d0e5a0516c641d06c92e37719148ed9c
Mohammad Varmazyar <mrvarmazyar@gmail.com>
2019-05-30 16:55:09 +0430

    configs : add db configs

--- src/configs.py
+++ src/configs.py
@@ -4,3 +4,11 @@ import os
 class APP:
     HOST = os.environ.get('APP_HOST', '0.0.0.0')
     PORT = os.environ.get('APP_PORT', 8080)
+
+
+class DB:
+    NAME = os.environ['DB_NAME']
+    HOST = os.environ['DB_HOST']
+    USER = os.environ['DB_USER']
+    PASSWORD = os.environ['DB_PASSWORD']
+    PORT = os.environ.get('DB_PORT', 5432)
