f11a3e42904558afaa05e65ad2961da00796c419
Mohammad Varmazyar <mrvarmazyar@gmail.com>
2019-05-30 16:53:58 +0430

    tests -> api : use `configs.py` to form the app url

--- src/tests/api_test.py
+++ src/tests/api_test.py
@@ -4,30 +4,34 @@ from http import HTTPStatus
 
 import requests
 
+import configs
 
-BASE_URL = 'http://localhost:8080{}'
+
+def _get_full_url(route):
+    return 'http://{host}:{port}{route}'.format(host=configs.APP.HOST,
+                                                port=configs.APP.PORT,
+                                                route=route)
 
 
 class APITests(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
-        requests.delete(BASE_URL.format('/hello/testUsername'))
+        requests.delete(_get_full_url('/hello/testUsername'))
 
     def test_not_found_user(self):
-        result = requests.get(BASE_URL.format('/hello/no_user'))
+        result = requests.get(_get_full_url('/hello/no_user'))
 
         self.assertEqual(result.status_code, HTTPStatus.NOT_FOUND)
 
     def test_invalid_username(self):
-        result = requests.post(BASE_URL.format('/hello'),
+        result = requests.post(_get_full_url('/hello'),
                                json={'username': 'user1'})
 
         self.assertEqual(result.status_code, HTTPStatus.BAD_REQUEST)
         self.assertIn('username', result.json())
 
     def test_invalid_birthday(self):
-        result = requests.post(BASE_URL.format('/hello'),
+        result = requests.post(_get_full_url('/hello'),
                                json={'username': 'testUsername',
                                      'birthday': '199-02-19'})
 
@@ -35,7 +39,7 @@ class APITests(unittest.TestCase):
         self.assertIn('birthday', result.json())
 
     def test_create_user(self):
-        result = requests.post(BASE_URL.format('/hello'),
+        result = requests.post(_get_full_url('/hello'),
                                json={'username': 'testUsername',
                                      'birthday': '1990-02-19'})
 
@@ -43,31 +47,31 @@ class APITests(unittest.TestCase):
         self.assertEqual(result.json()['username'], 'testUsername')
 
     def test_duplicate_user(self):
-        result = requests.post(BASE_URL.format('/hello'),
+        result = requests.post(_get_full_url('/hello'),
                                json={'username': 'testUsername',
                                      'birthday': '1990-02-19'})
 
         self.assertEqual(result.status_code, HTTPStatus.CONFLICT)
 
     def test_get_user(self):
-        result = requests.get(BASE_URL.format('/hello/testUsername'))
+        result = requests.get(_get_full_url('/hello/testUsername'))
 
         self.assertEqual(result.status_code, HTTPStatus.OK)
         self.assertIn('testUsername', result.json()['message'])
 
     def test_update_not_found_user(self):
-        result = requests.put(BASE_URL.format('/hello/no_user'))
+        result = requests.put(_get_full_url('/hello/no_user'))
 
         self.assertEqual(result.status_code, HTTPStatus.NOT_FOUND)
 
     def test_update_invalid_date_user(self):
-        result = requests.put(BASE_URL.format('/hello/testUsername'),
+        result = requests.put(_get_full_url('/hello/testUsername'),
                               json={'birthday': str(date.today())})
 
         self.assertEqual(result.status_code, HTTPStatus.BAD_REQUEST)
 
     def test_update_user(self):
-        result = requests.put(BASE_URL.format('/hello/testUsername'),
+        result = requests.put(_get_full_url('/hello/testUsername'),
                               json={'birthday': '1990-03-19'})
 
         self.assertEqual(result.status_code, HTTPStatus.NO_CONTENT)
@@ -76,12 +80,12 @@ class APITests(unittest.TestCase):
 class DeleteAPITest(unittest.TestCase):
 
     def test_delete_not_found_user(self):
-        result = requests.delete(BASE_URL.format('/hello/no_user'))
+        result = requests.delete(_get_full_url('/hello/no_user'))
 
         self.assertEqual(result.status_code, HTTPStatus.NOT_FOUND)
 
     def test_delete_user(self):
-        result = requests.delete(BASE_URL.format('/hello/testUsername'))
+        result = requests.delete(_get_full_url('/hello/testUsername'))
 
         self.assertEqual(result.status_code, HTTPStatus.NO_CONTENT)
 
